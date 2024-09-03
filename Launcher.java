68bc459cef98061815f88870bb667df55fbc1c7d
Juan <jfwilchesg@gmail.com>
2024-09-03 13:49:54 -0500

    Se agrega Clase Vista

--- Launcher.java
+++ Launcher.java
@@ -1,5 +1,13 @@
-public class launcher {
+public class Launcher {
     public static void main(String[] args) {
-        System.out.println("Hola, mundo!");
+        Remiso remiso = new Remiso("123456789", "Juan", "Perez");
+        Reservista reservista = new Reservista("987654321", "Ana", "Garcca", "xxxx-xxxx-xxxx");
+        Aplazado aplazado = new Aplazado("112233445", "Carlos", "Lopez", "xx/xx/xxxx");
+        Reclutado reclutado = new Reclutado("556677889", "Marta", "Rodriguez", "xxxxxxxxx");
+
+        System.out.println(remiso.toString());
+        System.out.println(reservista.toString());
+        System.out.println(aplazado.toString());
+        System.out.println(reclutado.toString());
     }
 }
