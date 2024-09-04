035902bd34ce85cec6e8b8a3db9ffd3bd119bed8
Santiago <santiago13012003@gmail.com>
2024-09-03 22:54:09 -0500

    primera etapa del Control

---
+++ Controlador.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Santiago Ramos Narvaez    cod: 20222020164
+ * @author Juan Felipe Wilches Gómez cod: 20231020137
+ * @author Solanghy Catalina Ortiz   cod: 20232020325
+ */
+public class Control {
+    
+    public void ingresarPersona(){
+        this.ingresarPersona = new ();
+    }
+            
+}
