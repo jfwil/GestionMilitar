966ee3ea3b14f356ca582e4bb1220ce2bd705786
Santiago <santiago13012003@gmail.com>
2024-09-04 11:01:06 -0500

    launcher del programa

--- Launcher.java
+++ Launcher.java
@@ -8,6 +8,9 @@ public class Launcher {
     
     public static void main(String[] arg){
         
+        Controlador controlador = new Controlador();
+        controlador.empezarPrograma();
+        
     }
     
 }
